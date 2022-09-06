cd4e6c115998b38473c7d38f9869582300ae921d
Caleb Edward King <king663@purdue.edu>
2022-09-06 14:17:05 -0400

    Lab 3

--- KingC_CGT215_Lab3/KingC_CGT215_Lab3.cpp
+++ KingC_CGT215_Lab3/KingC_CGT215_Lab3.cpp
@@ -2,11 +2,56 @@
 //
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "Your Selection: ";
+	cin >> choice;
 }
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+}
+
+void firstChoice(float A, float B) {
+	cout << A << "+" << B << "=" << (A + B) << endl;
+}
+void secondChoice(float A, float B) {
+	cout << A << "-" << B << "=" << (A - B) << endl;
+}
+void thirdChoice(float A, float B) {
+	cout << A << "*" << B << "=" << (A * B) << endl;
+}
+void fourthChoice(float A, float B) {
+	cout << A << "/" << B << "=" << (A / B) << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
+	}
+	return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
